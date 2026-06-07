39100a509cb84f1777c27dc478cff4280be48f7e
suhassajja07-hash <suhas.sajja07@gmail.com>
2026-06-07 08:58:46 +0100

    Add files via upload

--- Connecting_Satellites/Connecting_Satellites.py
+++ Connecting_Satellites/Connecting_Satellites.py
@@ -7,6 +7,8 @@ TITLE="Connect the satellites"
 
 num_of_satellites=7
 satellites={}
+lines=[]
+current_satellite=1
 
 for i in range(num_of_satellites):
     actor=Actor("satellite") 
@@ -19,7 +21,27 @@ def draw():
     screen.blit("background",(0,0))
     for i in satellites:
         satellites[i].draw()
-        screen.draw.text(str(i),satellites[i].pos)
+        screen.draw.text(str(i),(satellites[i].x-25,satellites[i].y-25))
+    for line in lines:
+        screen.draw.line(line[0],line[1],"white")
+
+def update():
+    pass
+    
+
+def on_mouse_down(pos):
+    global lines, current_satellite
+    if satellites[current_satellite].collidepoint(pos):
+        if current_satellite !=1:
+            starting_point=satellites[current_satellite].pos
+            endpoint=satellites[current_satellite-1].pos
+            lines.append([starting_point,endpoint])
+        current_satellite=current_satellite+1
+    else:
+        current_satellite=1
+        lines=[]
+
+
 
 
 pgzrun.go()
