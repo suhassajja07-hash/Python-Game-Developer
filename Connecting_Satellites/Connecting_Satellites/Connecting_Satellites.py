c90772af56137fffbe2d38cb3d4639e938b4774f
suhassajja07-hash <suhas.sajja07@gmail.com>
2026-06-14 08:54:10 +0100

    Add files via upload

---
+++ Connecting_Satellites/Connecting_Satellites/Connecting_Satellites.py
@@ -0,0 +1,56 @@
+import pgzrun
+import random
+import time
+
+WIDTH=700
+HEIGHT=700
+TITLE="Connect the satellites"
+
+num_of_satellites=7
+satellites={}
+lines=[]
+current_satellite=1
+
+for i in range(num_of_satellites):
+    actor=Actor("satellite") 
+    actor.x=random.randint(50,HEIGHT-50)
+    actor.y=random.randint(50,HEIGHT-50)
+    satellites[i+1]=actor
+print(satellites)
+start_time=time.time()
+timer=0
+
+def draw():
+    screen.blit("background",(0,0))
+    for i in satellites:
+        satellites[i].draw()
+        screen.draw.text(str(i),(satellites[i].x-25,satellites[i].y-25))
+    for line in lines:
+        screen.draw.line(line[0],line[1],"white")
+    screen.draw.text(str(timer),(WIDTH-50,50))
+
+def update():
+    global timer
+    if current_satellite<=num_of_satellites:
+
+        timer=round(time.time()-start_time,2)
+    
+    
+
+def on_mouse_down(pos):
+    global lines, current_satellite
+    if satellites[current_satellite].collidepoint(pos):
+        if current_satellite !=1:
+            starting_point=satellites[current_satellite].pos
+            endpoint=satellites[current_satellite-1].pos
+            lines.append([starting_point,endpoint])
+        current_satellite=current_satellite+1
+    else:
+        current_satellite=1
+        lines=[]
+
+
+
+
+pgzrun.go()
+    
