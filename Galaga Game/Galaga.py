37e7a5fc870837e91992bfc2c65fc7ceb357e40d
suhassajja07-hash <suhas.sajja07@gmail.com>
2026-07-15 17:39:12 +0100

    Add files via upload

--- Galaga Game/Galaga.py
+++ Galaga Game/Galaga.py
@@ -9,23 +9,68 @@ game_state="start"
 ship=Actor("ship")
 ship.pos=(WIDTH/2,HEIGHT-50)
 
+enemies=[]
+y=50
+
+
+for i in range (5):
+    row_of_enemy=[]
+    x=50
+    
+
+    for e in range (5):
+        enemy=Actor("enemy")
+        enemy.y=y
+        enemy.x=x
+        
+        row_of_enemy.append(enemy)
+
+        x=x+60
+            
+    enemies.append(row_of_enemy)
+    y=y+50
+    
+
+print(enemies)
+
+
 def draw():
     screen.fill("sky blue")
     if game_state=="start":
         screen.draw.text("WELCOME TO THE GALAGA GAME",center=(WIDTH/2,HEIGHT/2),fontsize=45, color="gold")
         screen.draw.text("Press any key to continue",center=(WIDTH/2,HEIGHT/2+50),fontsize=15, color="red")
+    
+
 
     elif game_state=="play":
         ship.draw()
-        
-        
-
+        for row in enemies:
+            for enemy in row:
+                enemy.draw()
 
 
 def on_key_down(key):
     global game_state
     if game_state=="start":
         game_state="play"
+
+    
+def update():
+    
+    if keyboard.left:
+        ship.x=ship.x-2
+
+    elif keyboard.right:
+        ship.x=ship.x+2
+
+    if ship.x<=0:
+        ship.x=WIDTH
+    
+    elif ship.x>=WIDTH:
+        ship.x=0
+
+
+        
 
 
 
